7f3f21451367fbe80f1ab4433cc25dc1961ffca3
zhamid01 <zhamid01@uw.edu>
2020-10-18 23:09:49 -0700

    Added a another array Compared array values

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -7,12 +7,25 @@ public class Problem2 {
     // Do not change signature (function name, parameters and return type)
     // Do not use extra space for another array. Solution to be in-pace with O(1) extra memory
     public static int remove(int[] nums) {
+
+        int tempArray[] = new int[nums.length - 1];
+        int placeHolder = 0;
         for (int i = 0; i < nums.length - 1; i++) {
+            int next = nums[i + 1];
+            int holder;
+
             if (nums.length - 1 == 0) {
                 return 1;
             }
-
+            else if (nums[i] == next) {
+                tempArray[placeHolder] = nums[i];
+                placeHolder++;
+            }
+            else if (nums[i] != next) {
+                tempArray[placeHolder] = nums[i];
+                tempArray[placeHolder++] = next;
+            }
         }
-        return nums.length; //PLACE HOLDER
+        return tempArray.length; //PLACE HOLDER
     }
 }
