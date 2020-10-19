b759af7b65a080f8a8a85133bef836a718ab71cb
zhamid01 <zhamid01@uw.edu>
2020-10-18 17:27:54 -0700

    Array length is 0

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -7,6 +7,12 @@ public class Problem2 {
     // Do not change signature (function name, parameters and return type)
     // Do not use extra space for another array. Solution to be in-pace with O(1) extra memory
     public static int remove(int[] nums) {
-        return Integer.MAX_VALUE; // place holder
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums.length - 1 == 0) {
+                return 1;
+            }
+
+        }
+        return nums.length; //PLACE HOLDER
     }
 }
