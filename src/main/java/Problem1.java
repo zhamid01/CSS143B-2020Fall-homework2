c215181162ac19999b2e23e04eb40cf3b6ebb237
zhamid01 <zhamid01@uw.edu>
2020-10-18 17:12:54 -0700

    Trying to sort array

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -6,5 +6,24 @@ public class Problem1 {
 
     // Do not change signature (function name, parameters type, and return type)
     public static void colorSort(int[] nums) {
+        int previous;
+        int next;
+        int holder;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            previous = nums[i - 1];
+            next = nums[i + 1];
+            if (previous <= nums[i])
+            {
+                continue;
+            }
+            else if (next < nums[i])
+            {
+                holder = nums[i];
+                nums[i] = next;
+                next = holder;
+            }
+        }
+
     }
 }
